54eee3bed08751c533220657799aefd125063da8
luanbsc <luanbscosta@hotmail.com>
2024-12-11 23:23:53 -0300

    Criação do menu principal

--- FlappyBird.py
+++ FlappyBird.py
@@ -40,6 +40,9 @@ w_b =pygame.transform.scale(w_b, (64, 64))
 # Carregando fontes
 fonte_numero = pygame.font.Font("Assets\\Font\\flappy-font-number.ttf", 30)
 fonte_texto = pygame.font.Font("Assets\\Font\\flappy-font-text.ttf", 48)
+fonte_texto_bg = pygame.font.Font("Assets\\Font\\flappy-font-text.ttf", 49)
+fonte_menu = pygame.font.Font("Assets\\Font\\Minecraft.ttf", 24)
+fonte_title_bg = pygame.font.Font("Assets\\Font\\flappy-font-text.ttf", 50)
 
 # Classe do Pássaro
 class Bird(pygame.sprite.Sprite):
@@ -85,9 +88,10 @@ class pipeBody(pygame.sprite.Sprite):
             self.rect.left = nmr_pipe
         if lower == 1:
             self.rect.bottom = 580
+            self.hitbox = pygame.Rect(self.rect.left + 4, self.rect.top, self.rect.w - 8, self.rect.h)
         else:
             self.rect.top = 0
-        self.hitbox = pygame.Rect(self.rect.left + 4, self.rect.top, self.rect.w - 8, self.rect.h)
+            self.hitbox = pygame.Rect(self.rect.left + 4, self.rect.top, self.rect.w - 8, self.rect.h)
 
 # Classe da cabeça dos pipes
 class pipeEnd(pygame.sprite.Sprite):
@@ -145,7 +149,6 @@ class TopPipe(pygame.sprite.Sprite):
 spritePlayer = pygame.sprite.Group()
 bird = Bird()
 spritePlayer.add(bird)
-bg_x1 = 0
 
 # Pipes
 pipes = pygame.sprite.Group() # Grupo dos pipes
@@ -196,8 +199,12 @@ pygame.time.set_timer(w_troca, 500)
 score = "0"
 
 # Variáveis de controle
+menu = True
 start = False
 conta_pontos = 0
+bg_x1 = 0
+title_x = 0
+title_sobe = True
 
 # Looping do jogo
 while running:
@@ -206,7 +213,7 @@ while running:
             w_p, w_b = w_b, w_p
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN and not menu:
             if event.key == pygame.K_w:
                 bird.move_up = True
                 if not start:
@@ -228,81 +235,82 @@ while running:
             lp3.update()
         bg_x1 -= 2
 
-    # Somar +1 na pontuação ao passar de um pipe
-    if lp1.pipe_body.rect.centerx <= screen.get_width()/4 and conta_pontos == 0:
-        score = str(int(score)+1)
-        conta_pontos = 1
+    if not menu:
+        # Somar +1 na pontuação ao passar de um pipe
+        if lp1.pipe_body.rect.centerx <= screen.get_width()/4 and conta_pontos == 0:
+            score = str(int(score)+1)
+            conta_pontos = 1
 
-    # Somar +1 na pontuação ao passar de um pipe
-    if lp2.pipe_body.rect.centerx <= screen.get_width()/4 and conta_pontos == 1:
-        score = str(int(score)+1)
-        conta_pontos = 2
+        # Somar +1 na pontuação ao passar de um pipe
+        if lp2.pipe_body.rect.centerx <= screen.get_width()/4 and conta_pontos == 1:
+            score = str(int(score)+1)
+            conta_pontos = 2
 
-    # Somar +1 na pontuação ao passar de um pipe
-    if lp3.pipe_body.rect.centerx <= screen.get_width()/4 and conta_pontos == 2:
-        score = str(int(score)+1)
-        conta_pontos = 0
+        # Somar +1 na pontuação ao passar de um pipe
+        if lp3.pipe_body.rect.centerx <= screen.get_width()/4 and conta_pontos == 2:
+            score = str(int(score)+1)
+            conta_pontos = 0
 
-    # Mudar o posicionamento do primeiro pipe após passar da tela
-    if lp1.pipe_body.rect.right <= 0:
-        lp1.kill()
-        tp1.kill()
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        lp1 = LowerPipe(lp3.pipe_body.rect.left+220)
-        pipes.add(lp1.pipe_body)
-        pipes.add(lp1.pipe_end)
-        tp1 = TopPipe(lp1.tamanho, lp3.pipe_body.rect.left+220)
-        pipes.add(tp1.pipe_body)
-        pipes.add(tp1.pipe_end)
-        rects_pipes.append(lp1.pipe_body)
-        rects_pipes.append(lp1.pipe_end)
-        rects_pipes.append(tp1.pipe_body)
-        rects_pipes.append(tp1.pipe_end)
+        # Mudar o posicionamento do primeiro pipe após passar da tela
+        if lp1.pipe_body.rect.right <= 0:
+            lp1.kill()
+            tp1.kill()
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            lp1 = LowerPipe(lp3.pipe_body.rect.left+220)
+            pipes.add(lp1.pipe_body)
+            pipes.add(lp1.pipe_end)
+            tp1 = TopPipe(lp1.tamanho, lp3.pipe_body.rect.left+220)
+            pipes.add(tp1.pipe_body)
+            pipes.add(tp1.pipe_end)
+            rects_pipes.append(lp1.pipe_body)
+            rects_pipes.append(lp1.pipe_end)
+            rects_pipes.append(tp1.pipe_body)
+            rects_pipes.append(tp1.pipe_end)
 
-    # Mudar o posicionamento do segundo pipe após passar da tela
-    if lp2.pipe_body.rect.right <= 0:
-        lp2.kill()
-        tp2.kill()
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        lp2 = LowerPipe(lp1.pipe_body.rect.left+220)
-        pipes.add(lp2.pipe_body)
-        pipes.add(lp2.pipe_end)
-        tp2 = TopPipe(lp2.tamanho, lp1.pipe_body.rect.left+220)
-        pipes.add(tp2.pipe_body)
-        pipes.add(tp2.pipe_end)
-        rects_pipes.append(lp2.pipe_body)
-        rects_pipes.append(lp2.pipe_end)
-        rects_pipes.append(tp2.pipe_body)
-        rects_pipes.append(tp2.pipe_end)
+        # Mudar o posicionamento do segundo pipe após passar da tela
+        if lp2.pipe_body.rect.right <= 0:
+            lp2.kill()
+            tp2.kill()
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            lp2 = LowerPipe(lp1.pipe_body.rect.left+220)
+            pipes.add(lp2.pipe_body)
+            pipes.add(lp2.pipe_end)
+            tp2 = TopPipe(lp2.tamanho, lp1.pipe_body.rect.left+220)
+            pipes.add(tp2.pipe_body)
+            pipes.add(tp2.pipe_end)
+            rects_pipes.append(lp2.pipe_body)
+            rects_pipes.append(lp2.pipe_end)
+            rects_pipes.append(tp2.pipe_body)
+            rects_pipes.append(tp2.pipe_end)
 
-    # Mudar o posicionamento do terceiro pipe após passar da tela
-    if lp3.pipe_body.rect.right <= 0:
-        lp3.kill()
-        tp3.kill()
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        rects_pipes.pop(0)
-        lp3 = LowerPipe(lp2.pipe_body.rect.left+220)
-        pipes.add(lp3.pipe_body)
-        pipes.add(lp3.pipe_end)
-        tp3 = TopPipe(lp3.tamanho, lp2.pipe_body.rect.left+220)
-        pipes.add(tp3.pipe_body)
-        pipes.add(tp3.pipe_end)
-        rects_pipes.append(lp3.pipe_body)
-        rects_pipes.append(lp3.pipe_end)
-        rects_pipes.append(tp3.pipe_body)
-        rects_pipes.append(tp3.pipe_end)
+        # Mudar o posicionamento do terceiro pipe após passar da tela
+        if lp3.pipe_body.rect.right <= 0:
+            lp3.kill()
+            tp3.kill()
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            rects_pipes.pop(0)
+            lp3 = LowerPipe(lp2.pipe_body.rect.left+220)
+            pipes.add(lp3.pipe_body)
+            pipes.add(lp3.pipe_end)
+            tp3 = TopPipe(lp3.tamanho, lp2.pipe_body.rect.left+220)
+            pipes.add(tp3.pipe_body)
+            pipes.add(tp3.pipe_end)
+            rects_pipes.append(lp3.pipe_body)
+            rects_pipes.append(lp3.pipe_end)
+            rects_pipes.append(tp3.pipe_body)
+            rects_pipes.append(tp3.pipe_end)
 
-    # Colisão
-    if pygame.Rect.collidelist(bird.hitbox, rects_pipes) != -1:
-        bird.game_over = True
+        # Colisão
+        if pygame.Rect.collidelist(bird.hitbox, rects_pipes) != -1:
+            bird.game_over = True
 
     # Reseta a posição do fundo para a imagem ficar em looping
     if bg_x1 <= -300:
@@ -320,19 +328,68 @@ while running:
     pipes.draw(screen)
 
     # Desenhar botão indicativo de iniciar o jogo
-    if not start:
+    if not start and not menu:
         screen.blit(w_b, (screen.get_width()/2 - 32, screen.get_height()/2))
-        getready_text = fonte_texto.render("Get Ready", False, (0, 0, 0))
+        getready_text = fonte_texto.render("Get Ready", False, (218,165,32))
         getready_text_rect = getready_text.get_rect(center=(screen.get_width()/2, screen.get_height()/6))
+        getready_text_bg = fonte_texto_bg.render("Get Ready", False, (184,134,11))
+        screen.blit(getready_text_bg, getready_text_rect)
         screen.blit(getready_text, getready_text_rect)
     # Desenhar o score
-    else:
+    elif start:
         score_text = fonte_numero.render(score, False, (0, 0, 0))
         score_text_rect = score_text.get_rect(center=(screen.get_width()/2, screen.get_height()/10))
         screen.blit(score_text, score_text_rect)
 
+    if menu:
+        # Título Flappy Bird
+        title_text = fonte_texto.render("Flappy Bird", False, (46, 139, 87))
+        title_text_rect = title_text.get_rect(center=(screen.get_width()/2 - 50, screen.get_height()/3 + title_x))
+        title_text_bg = fonte_title_bg.render("Flappy Bird", False, (173,255,47))
+        title_text_bg_rect = title_text_bg.get_rect(center=(screen.get_width()/2 - 50, screen.get_height()/3 + title_x))
+        screen.blit(title_text, title_text_rect)
+        screen.blit(title_text_bg, title_text_bg_rect)
+        bird.rect.center = (screen.get_width() - screen.get_width()/6, screen.get_height()/3 + title_x)
+
+        # Botão de Start
+        pygame.draw.rect(screen, (160,82,45), pygame.Rect(screen.get_width()/6, screen.get_height() - screen.get_height()/5, 100, 40))
+        pygame.draw.rect(screen, (250,240,230), pygame.Rect(screen.get_width()/6 + 2, screen.get_height() - screen.get_height()/5 + 2, 96, 36))
+        pygame.draw.rect(screen, (210,105,30), pygame.Rect(screen.get_width()/6 + 6, screen.get_height() - screen.get_height()/5 + 6, 88, 28))
+        start_text = fonte_menu.render("Start", False, (250,240,230))
+        start_text_rect = start_text.get_rect(left=screen.get_width()/6 + 19, top=screen.get_height() - screen.get_height()/5 + 10)
+        screen.blit(start_text, start_text_rect)
+
+        # Botão de Score
+        pygame.draw.rect(screen, (160,82,45), pygame.Rect(270, screen.get_height() - screen.get_height()/5, 100, 40))
+        pygame.draw.rect(screen, (250,240,230), pygame.Rect(270 + 2, screen.get_height() - screen.get_height()/5 + 2, 96, 36))
+        pygame.draw.rect(screen, (210,105,30), pygame.Rect(270 + 6, screen.get_height() - screen.get_height()/5 + 6, 88, 28))
+        score_text = fonte_menu.render("Score", False, (250,240,230))
+        score_text_rect = score_text.get_rect(left=270 + 15, top=screen.get_height() - screen.get_height()/5 + 10)
+        screen.blit(score_text, score_text_rect)
+
+        # Checagem de clicar no Start
+        if pygame.mouse.get_pressed()[0]:
+            if pygame.Rect(screen.get_width()/6, screen.get_height() - screen.get_height()/5, 100, 40).collidepoint(pygame.mouse.get_pos()):
+                bird.rect.center = (screen.get_width()/4, screen.get_height()/2)
+                menu = False
+        
+        # Checagem de clicar no Score
+        if pygame.mouse.get_pressed()[0]:
+            if pygame.Rect(270, screen.get_height() - screen.get_height()/5, 100, 40).collidepoint(pygame.mouse.get_pos()):
+                pass
+
+        if title_x >= 20:
+            title_sobe = True
+        elif title_x <= -20:
+            title_sobe = False
+
+        if title_sobe:    
+            title_x -= 1
+        else:
+            title_x += 1
+
     # Desenhos das hitbox para teste
-    pygame.draw.rect(screen, (100, 0, 0), bird.hitbox, 2)      # Hitbox do pássaro
+    #pygame.draw.rect(screen, (100, 0, 0), bird.hitbox, 2)      # Hitbox do pássaro
 
     #pygame.draw.rect(screen, (0, 0, 255), lp1.pipe_body.hitbox, 2)
     #pygame.draw.rect(screen, (100, 100, 255), lp1.pipe_end.hitbox, 2)
@@ -343,6 +400,11 @@ while running:
     #pygame.draw.rect(screen, (100, 100, 255), lp2.pipe_end.hitbox, 2)
     #pygame.draw.rect(screen, (0, 0, 255), tp2.pipe_body.hitbox, 2)
     #pygame.draw.rect(screen, (100, 100, 255), tp2.pipe_end.hitbox, 2)
+        
+    #pygame.draw.rect(screen, (0, 0, 255), lp3.pipe_body.hitbox, 2)
+    #pygame.draw.rect(screen, (100, 100, 255), lp3.pipe_end.hitbox, 2)
+    #pygame.draw.rect(screen, (0, 0, 255), tp3.pipe_body.hitbox, 2)
+    #pygame.draw.rect(screen, (100, 100, 255), tp3.pipe_end.hitbox, 2)
 
     # Atualizar tela
     pygame.display.flip()
